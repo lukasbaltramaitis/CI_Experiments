b61a20045c610f99a2bb5a1917b55eddc2ecb3e3
baltrama <lukas.baltramaitis@ut.ee>
2023-04-12 16:49:32 +0300

    Prepared bpic17 data added.

--- CI_Experiments/preparation_code/preparation.py
+++ CI_Experiments/preparation_code/preparation.py
@@ -1,12 +1,27 @@
 from typing import List
 import pandas as pd
+import networkx as nx
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 
 CASE_ID = 'Case ID'
 ACTIVITY = 'Activity'
 END = 'End'
 SEP = ';'
+POS = 'Pos'
+NEG = 'Neg'
+OUTCOME = 'Outcome'
+
+
+def replace_outcome_with_pos_neg(data: pd.DataFrame) -> pd.DataFrame:
+     data = data.copy()
+     data[POS] = data[OUTCOME]
+     data[NEG] = 1 - data[OUTCOME]
+
+     data = data.drop(columns=[OUTCOME])
+     
+     return data
 
 
 def read_csv(path: str, sep=SEP) -> pd.DataFrame:
@@ -20,16 +35,24 @@ def save_csv(data: pd.DataFrame, path: str):
 class Preparation:
     def __init__(self, data: pd.DataFrame):
         self.data = data
+        self.graph_set = set()
+        self.graph = None
+
+
+    def _join_to_case(self, case: pd.DataFrame):
+        del case[CASE_ID]
+        return case.max()
 
 
     def encode(self, columns_to_encode: List[str]):
         if self.data is not None:
-            data = data.copy()
+            self.encoded_data = self.data.copy()
             for column in columns_to_encode:
                 ohe = OneHotEncoder()
-                encoded = pd.DataFrame(ohe.fit_transform(self.data[[column]]).toarray())
-                self.data = self.data.join(encoded)
-            self.data = self.data.drop(columns=columns_to_encode)
+                encoded = pd.DataFrame(ohe.fit_transform(self.encoded_data[[column]]).toarray(), columns=ohe.get_feature_names_out())
+                self.encoded_data = self.encoded_data.join(encoded)
+            self.encoded_data = self.encoded_data.drop(columns=columns_to_encode)
+            self.encoded_data = self.encoded_data.groupby(CASE_ID).apply(self._join_to_case)
     
 
     def _add_activities_to_columns(self, case: pd.DataFrame) -> pd.DataFrame:
@@ -40,10 +63,7 @@ class Preparation:
 
 
     def encode_with_activity_seq(self, columns_to_encode: List[str]):
-        def get_case_length(case: pd.DataFrame) -> int:
-            return case.shape[0]
-        
-        nr_of_cols = max(self.data.groupby(CASE_ID).apply(get_case_length))
+        nr_of_cols = self.data[CASE_ID].value_counts().max()
         self.new_cols = []
         for i in range(nr_of_cols):
             column = f'N{i}'
@@ -51,7 +71,6 @@ class Preparation:
             self.data[column] = END
 
         
-
         self.data = self.data.groupby(CASE_ID).apply(self._add_activities_to_columns)
         self.data = self.data.drop(columns=[ACTIVITY])
         columns_to_encode.remove(ACTIVITY)
@@ -60,25 +79,18 @@ class Preparation:
 
 
     def _save_graph(self, path):
-        edges = [(key, val) for key, vals in self.graph.items() for val in vals]
-        df = pd.DataFrame(edges)
-        adj_matrix = pd.crosstab(df[0], df[1])
+        G = nx.from_edgelist(self.graph_set)
+        self.graph = G
+        adj_matrix = nx.to_pandas_adjacency(G)
         save_csv(adj_matrix, path)
 
-    def create_activity_graph(self, path):
-        def add_relationships_from_the_case(case: pd.DataFrame):
-            nr_of_rows = case.shape[0]
-            for i in range(nr_of_rows - 1):
-                activity_cause = case.iloc[i][ACTIVITY]
-                activity_effect = case.iloc[i+1][ACTIVITY]
-                graph_values = self.graph.get(activity_cause, None)
-                if graph_values is not None:
-                    graph_values.add(activity_effect)
-                    self.graph[activity_cause] = graph_values
-                else:
-                    self.graph[activity_cause] = set(activity_effect)
 
+    def create_activity_graph(self, path):
         activity_data = self.data[[CASE_ID, ACTIVITY]]
-        self.graph = {}
-        activity_data.groupby(ACTIVITY).apply(add_relationships_from_the_case)
+        activity_data['case_id_1'] = activity_data[CASE_ID].shift(-1)
+        activity_data['activity_effect'] = activity_data[ACTIVITY].shift(-1)
+        activity_data = activity_data.iloc[:-1]
+        activity_data['edge'] = list(zip(activity_data[ACTIVITY], activity_data['activity_effect']))
+        activity_data = activity_data[activity_data[CASE_ID] == activity_data['case_id_1']]
+        self.graph_set = set(activity_data['edge'].tolist())
         self._save_graph(path)
